c18d6ea508030a6dc3463b21e8d6bb9c90e0fbf4
Jegis9 <ixcaquic22@gmailcom>
2024-10-09 22:11:26 -0600

    Reportes agregados

--- Aplicaciones/Emergencias/urls.py
+++ Aplicaciones/Emergencias/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from . import views
-from .views import ServicioCreateView, ServicioListView, VariosListView,AmbulanciaListView,IncendiosListView
 
 urlpatterns = [
-    path('add/',ServicioCreateView.as_view(), name='add'),
-    path('lista_servicios/', ServicioListView.as_view(), name='lista_servicios'),
-    path('varios/', VariosListView.as_view(), name='varios'),
-    path('ambulancia/', AmbulanciaListView.as_view(), name='ambulancia'),
-    path('incendios/', IncendiosListView.as_view(), name='incendios'),
+    path('add/',views.servicio_create_view, name='add'),
+    path('lista_servicios/', views.servicio_list_view, name='lista_servicios'),
+    path('varios/', views.varios_list_view, name='varios'),
+    path('ambulancia/', views.ambulancia_list_view, name='ambulancia'),
+    path('incendios/', views.incendios_list_view, name='incendios'),
+    path('ambulancia/<int:ambulancia_id>/reporte/', views.generar_reporte_ambulancia, name='generar_reporte_ambulancia'),
+    path('varios/<int:varios_id>/reporte/', views.generar_reporte_servicios_varios, name='generar_reporte_varios'),
+    path('incendios/<int:incendio_id>/reporte/', views.generar_reporte_incendios, name='generar_reporte_incendios'),
+    
+    
     # path('servicios/', views.lista_servicios, name='lista_servicios'),  # Nueva URL
     # path('servicios/varios/', views.lista_varios, name='lista_varios'),
     # path('servicios/ambulancia/', views.lista_ambulancia, name='lista_ambulancia'),
